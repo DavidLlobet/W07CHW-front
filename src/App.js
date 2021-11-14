4492371c6d41b045c89cd18986274e30a3452d55
David <david.llobet6@gmail.com>
2021-11-14 19:25:21 +0100

    Add SocialPage component to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import useUsers from "./hooks/useUsers";
+
+import SocialPage from "./pages/SocialPage";
+import { paths } from "./paths/paths";
 
 function App() {
+  const { loadUsers } = useUsers();
+
+  useEffect(() => {
+    loadUsers();
+  }, [loadUsers]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Router>
+        {/* <Header /> */}
+        <Switch>
+          <Route path={paths.socialPage} exact>
+            <SocialPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
