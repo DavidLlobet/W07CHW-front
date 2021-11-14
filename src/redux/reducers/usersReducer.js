e42d4ad74dbc0f980af0af804115a97ba4499620
David <david.llobet6@gmail.com>
2021-11-14 13:48:26 +0100

    Create usersReducer with loadUsers case

---
+++ src/redux/reducers/usersReducer.js
@@ -0,0 +1,18 @@
+import actionTypes from "../actions/actionTypes";
+
+const usersReducer = (users = [], action) => {
+  let newUsers;
+
+  switch (action.type) {
+    case actionTypes.loadUsers:
+      newUsers = [...action.users];
+      break;
+
+    default:
+      newUsers = users;
+      break;
+  }
+  return newUsers;
+};
+
+export default usersReducer;
