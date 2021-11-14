c65d247005a9462dd7651bb0dc3143e53f07592f
David <david.llobet6@gmail.com>
2021-11-14 20:33:36 +0100

    Create LoginPage

---
+++ src/pages/LoginPage/LoginPage.js
@@ -0,0 +1,13 @@
+import LoginForm from "../../components/LoginForm/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <>
+      <main className="main">
+        <LoginForm />
+      </main>
+    </>
+  );
+};
+
+export default LoginPage;
