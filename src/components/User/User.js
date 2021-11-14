0f4f7ce08db3df62f1a36903e91b72ed172b8512
David <david.llobet6@gmail.com>
2021-11-14 18:40:19 +0100

    Create test for User component

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -4,13 +4,13 @@ const User = ({ name, age, bio, image }) => {
       <h2 className="fw-light text-center display-5">{name}</h2>
       <img
         src={image}
-        alt="robot"
+        alt="user"
         width="300"
         height="300"
         className="bd-placeholder-img card-img-top"
       ></img>
-      <p className="robot__features-name">Edad: {age}</p>
-      <p className="robot__features-name">Biografía: {bio}</p>
+      <p>Edad: {age}</p>
+      <p>Biografía: {bio}</p>
       <button className="button">UPDATE</button>
       <button className="button">DELETE</button>
     </article>
