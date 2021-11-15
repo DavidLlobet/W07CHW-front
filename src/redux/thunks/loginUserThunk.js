ebaa0474e54657bcd43e6d8e33d38afa2e0876cd
David <david.llobet6@gmail.com>
2021-11-15 18:18:55 +0100

    Add headers to response in loadUsersThunk

--- src/redux/thunks/loginUserThunk.js
+++ src/redux/thunks/loginUserThunk.js
@@ -7,7 +7,6 @@ const urlApi = "https://w07chw.herokuapp.com/users";
 export const loginUserThunk = (user) => async (dispatch) => {
   try {
     const { data, status } = await axios.post(urlApi + "/login", user);
-    console.log(data);
     const token = data.token;
     if (status === 200) {
       const user = jwtDecode(token);
--- src/redux/thunks/userThunk.js
+++ src/redux/thunks/userThunk.js
@@ -4,10 +4,11 @@ import { loadUsersAction } from "../actions/actionCreators";
 const urlApi = "https://w07chw.herokuapp.com/users/users";
 
 export const loadUsersThunk = () => async (dispatch) => {
-  //   const token =
-  //     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOGZlMGNjMGQ5MDBhZDEwY2QxODVlYSIsIm5hbWUiOiJTaXIgQm9vbGVhbiIsImlhdCI6MTYzNjg5MTYwOSwiZXhwIjoxNjM3MTUwODA5fQ.PGI5ZiAWQcg_0DdqGSsvblGiOqii9ZuXUaRD6yf-qG4";
+  const token = localStorage.getItem("David");
 
-  const response = await axios.get(urlApi);
+  const response = await axios.get(urlApi, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
   const users = await response.data;
   dispatch(loadUsersAction(users));
 };
